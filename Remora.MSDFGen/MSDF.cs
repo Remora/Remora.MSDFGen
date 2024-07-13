27a81a31e355e3c42ecbd46e48c8c5fd85445009
Jarl Gullberg <jarl.gullberg@gmail.com>
2024-07-13 23:11:10 +0200

    Merge partial class.

--- Remora.MSDFGen/MSDF.cs
+++ Remora.MSDFGen/MSDF.cs
@@ -18,7 +18,7 @@ internal struct MultiDistance
     public double med;
 }
 
-public static partial class MSDF
+public static class MSDF
 {
     public static bool PixelClash(Color4 a, Color4 b, double threshold)
     {
@@ -668,5 +668,260 @@ public static partial class MSDF
         }
 
         return new Color3((float)(msd.r / range) + 0.5f, (float)(msd.g / range) + 0.5f, (float)(msd.b / range) + 0.5f);
+    }
+
+    private static EdgeColor[] switchColors = { EdgeColor.Cyan, EdgeColor.Magenta, EdgeColor.Yellow };
+
+    private static void SwitchColor(ref EdgeColor color, ref ulong seed, EdgeColor banned)
+    {
+        var combined = color & banned;
+
+        if (combined is EdgeColor.Red or EdgeColor.Green or EdgeColor.Blue)
+        {
+            color = combined ^ EdgeColor.White;
+            return;
+        }
+
+        if (color is EdgeColor.Black or EdgeColor.White)
+        {
+            color = switchColors[seed % 3];
+            seed /= 3;
+            return;
+        }
+
+        var shifted = (int)color << (int)(1 + (seed & 1));
+        color = (EdgeColor)((shifted | (shifted >> 3)) & (int)EdgeColor.White);
+        seed >>= 1;
+    }
+
+    public static void EdgeColoringSimple(Shape shape, double angleThreshold, ulong seed)
+    {
+        var crossThreshold = Math.Sin(angleThreshold);
+        var corners = new List<int>();
+        foreach (var contour in shape.Contours)
+        {
+            corners.Clear();
+
+            if (contour.Edges.Count != 0)
+            {
+                var prevDirection = contour.Edges[^1].GetDirection(1);
+
+                for (var j = 0; j < contour.Edges.Count; j++)
+                {
+                    var edge = contour.Edges[j];
+                    if (IsCorner(
+                            Vector2.Normalize(prevDirection),
+                            Vector2.Normalize(edge.GetDirection(0)),
+                            crossThreshold))
+                    {
+                        corners.Add(j);
+                    }
+
+                    prevDirection = edge.GetDirection(1);
+                }
+            }
+
+            switch (corners.Count)
+            {
+                case 0:
+                {
+                    foreach (var edge in contour.Edges)
+                    {
+                        edge.Color = EdgeColor.White;
+                    }
+
+                    break;
+                }
+                case 1:
+                {
+                    EdgeColor[] colors = { EdgeColor.White, EdgeColor.White, EdgeColor.Black };
+                    SwitchColor(ref colors[0], ref seed, EdgeColor.Black);
+                    SwitchColor(ref colors[2], ref seed, EdgeColor.Black);
+
+                    var corner = corners[0];
+
+                    switch (contour.Edges.Count)
+                    {
+                        case >= 3:
+                        {
+                            var m = contour.Edges.Count;
+                            for (var j = 0; j < m; j++)
+                            {
+                                var magic = (int)(3 + (2.875f * j / (m - 1)) - 1.4375f + 0.5f) - 3; //see edge-coloring.cpp in the original msdfgen
+                                contour.Edges[(corner + j) % m].Color = colors[1 + magic];
+                            }
+
+                            break;
+                        }
+                        case >= 1:
+                        {
+                            var parts = new EdgeSegment[7];
+                            contour.Edges[0].SplitInThirds(
+                                out parts[0 + (3 * corner)],
+                                out parts[1 + (3 * corner)],
+                                out parts[2 + (3 * corner)]);
+                            if (contour.Edges.Count >= 2)
+                            {
+                                contour.Edges[1].SplitInThirds(
+                                    out parts[3 - (3 * corner)],
+                                    out parts[4 - (3 * corner)],
+                                    out parts[5 - (3 * corner)]);
+                                parts[0].Color = colors[0];
+                                parts[1].Color = colors[0];
+                                parts[2].Color = colors[1];
+                                parts[3].Color = colors[1];
+                                parts[4].Color = colors[2];
+                                parts[5].Color = colors[2];
+                            }
+                            else
+                            {
+                                parts[0].Color = colors[0];
+                                parts[1].Color = colors[1];
+                                parts[2].Color = colors[2];
+                            }
+
+                            contour.Edges.Clear();
+                            for (var j = 0; parts[j] != null; j++)
+                            {
+                                contour.Edges.Add(parts[j]);
+                            }
+
+                            break;
+                        }
+                    }
+
+                    break;
+                }
+                default:
+                {
+                    var cornerCount = corners.Count;
+                    var spline = 0;
+                    var start = corners[0];
+                    var m = contour.Edges.Count;
+                    var color = EdgeColor.White;
+                    SwitchColor(ref color, ref seed, EdgeColor.Black);
+                    var initialColor = color;
+                    for (var j = 0; j < m; j++)
+                    {
+                        var index = (start + j) % m;
+                        if (spline + 1 < cornerCount && corners[spline + 1] == index)
+                        {
+                            spline++;
+                            SwitchColor(
+                                ref color,
+                                ref seed,
+                                (EdgeColor)((spline == cornerCount - 1 ? 1 : 0) * (int)initialColor));
+                        }
+
+                        contour.Edges[index].Color = color;
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private static bool IsCorner(Vector2 aDir, Vector2 bDir, double crossThreshold)
+    {
+        return Vector2.Dot(aDir, bDir) <= 0 || Math.Abs(EdgeSegment.Cross(aDir, bDir)) > crossThreshold;
+    }
+
+	private struct Context
+    {
+        public Vector2 position;
+        public Shape shape;
+        public Contour contour;
+    }
+
+    private static Vector2 ToVector2(FTVector vector)
+    {
+        return new Vector2(vector.X.Value / 64f, vector.Y.Value / 64f);
+    }
+
+    private static int MoveTo(ref FTVector to, IntPtr user)
+    {
+        unsafe
+        {
+            var context = Unsafe.Read<Context>((void*)user);
+            var contour = new Contour();
+            context.shape.Contours.Add(contour);
+            context.contour = contour;
+            context.position = ToVector2(to);
+            Unsafe.Write((void*)user, context);
+            return 0;
+        }
+    }
+
+    private static int LineTo(ref FTVector to, IntPtr user)
+    {
+        unsafe
+        {
+            var context = Unsafe.Read<Context>((void*)user);
+            context.contour.Edges.Add(new LinearSegment(context.position, ToVector2(to), EdgeColor.White));
+            context.position = ToVector2(to);
+            Unsafe.Write((void*)user, context);
+            return 0;
+        }
+    }
+
+    private static int ConicTo(ref FTVector control, ref FTVector to, IntPtr user)
+    {
+        unsafe
+        {
+            var context = Unsafe.Read<Context>((void*)user);
+            context.contour.Edges.Add(
+                new QuadraticSegment(context.position, ToVector2(control), ToVector2(to), EdgeColor.White));
+            context.position = ToVector2(to);
+            Unsafe.Write((void*)user, context);
+            return 0;
+        }
+    }
+
+    private static int CubicTo(ref FTVector control1, ref FTVector control2, ref FTVector to, IntPtr user)
+    {
+        unsafe
+        {
+            var context = Unsafe.Read<Context>((void*)user);
+            context.contour.Edges.Add(
+                new CubicSegment(
+                    context.position,
+                    ToVector2(control1),
+                    ToVector2(control2),
+                    ToVector2(to),
+                    EdgeColor.White));
+            context.position = ToVector2(to);
+            Unsafe.Write((void*)user, context);
+            return 0;
+        }
+    }
+
+    public static Shape LoadGlyph(Face face, int unicode)
+    {
+        if (face == null)
+        {
+            throw new ArgumentNullException(nameof(face));
+        }
+
+        face.LoadChar((uint)unicode, LoadFlags.NoScale, LoadTarget.Normal);
+
+        var output = new Shape();
+
+        var context = new Context
+        {
+            shape = output
+        };
+
+        OutlineFuncs funcs = new OutlineFuncs();
+        funcs.MoveFunction = MoveTo;
+        funcs.LineFunction = LineTo;
+        funcs.ConicFunction = ConicTo;
+        funcs.CubicFunction = CubicTo;
+        unsafe
+        {
+            face.Glyph.Outline.Decompose(funcs, (IntPtr)Unsafe.AsPointer(ref context));
+        }
+
+        return output;
     }
 }
