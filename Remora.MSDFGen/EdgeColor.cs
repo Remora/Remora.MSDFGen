38516766a1cee45528de6ecdbffea300dc68a344
Jarl Gullberg <jarl.gullberg@gmail.com>
2024-07-13 23:21:26 +0200

    Move EdgeColor to its own file.

---
+++ Remora.MSDFGen/EdgeColor.cs
@@ -0,0 +1,19 @@
+//
+//  SPDX-FileName: EdgeColor.cs
+//  SPDX-FileCopyrightText: Copyright (c) Jarl Gullberg
+//  SPDX-License-Identifier: MIT
+//
+
+namespace Remora.MSDFGen;
+
+public enum EdgeColor
+{
+    Black = 0,
+    Red = 1,
+    Green = 2,
+    Yellow = 3,
+    Blue = 4,
+    Magenta = 5,
+    Cyan = 6,
+    White = 7
+}
--- Remora.MSDFGen/EdgeSegment.cs
+++ Remora.MSDFGen/EdgeSegment.cs
@@ -9,18 +9,6 @@ using System.Numerics;
 
 namespace Remora.MSDFGen;
 
-public enum EdgeColor
-{
-    Black = 0,
-    Red = 1,
-    Green = 2,
-    Yellow = 3,
-    Blue = 4,
-    Magenta = 5,
-    Cyan = 6,
-    White = 7
-}
-
 public abstract class EdgeSegment
 {
     public EdgeColor Color { get; set; }
