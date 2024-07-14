bbdb86c6977ba0879a707a45e6cd4ea63b90fcc7
Jarl Gullberg <jarl.gullberg@gmail.com>
2024-07-14 12:08:47 +0200

    Document and rework EdgeColor enumeration.

--- Remora.MSDFGen/EdgeColor.cs
+++ Remora.MSDFGen/EdgeColor.cs
@@ -4,16 +4,53 @@
 //  SPDX-License-Identifier: MIT
 //
 
+using System;
+
 namespace Remora.MSDFGen;
 
+/// <summary>
+/// Represents possible colours for an edge in a contour.
+/// </summary>
+[Flags]
 public enum EdgeColor
 {
+    /// <summary>
+    /// Represents a colourless edge.
+    /// </summary>
     Black = 0,
-    Red = 1,
-    Green = 2,
-    Yellow = 3,
-    Blue = 4,
-    Magenta = 5,
-    Cyan = 6,
-    White = 7
+
+    /// <summary>
+    /// Represents a red edge.
+    /// </summary>
+    Red = 1 << 1,
+
+    /// <summary>
+    /// Represents a green edge.
+    /// </summary>
+    Green = 1 << 2,
+
+    /// <summary>
+    /// Represents a blue edge.
+    /// </summary>
+    Blue = 1 << 3,
+
+    /// <summary>
+    /// Represents a yellow edge, which is the bitwise combination of a red and green edge.
+    /// </summary>
+    Yellow = Red | Green,
+
+    /// <summary>
+    /// Represents a magenta edge, which is the bitwise combination of a red and blue edge.
+    /// </summary>
+    Magenta = Red | Blue,
+
+    /// <summary>
+    /// Represents a cyan edge, which is the bitwise combination of a green and blue edge.
+    /// </summary>
+    Cyan = Green | Blue,
+
+    /// <summary>
+    /// Represents a white edge, which is the bitwise combination of a red, green, and blue edge.
+    /// </summary>
+    White = Red | Green | Blue
 }
